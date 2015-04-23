53dd010886e7e3277be19896c5550651c2dea64f
Mateus <mateusboga@gmail.com>
2015-04-23 02:39:42 +0100

    update2

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 	var canvas = document.getElementById('mainc');
 	var C = {width:0,height:0,fps:60}; C.width = canvas.width; C.height = canvas.height; C.centerX = C.width/2; C.centerY = C.height/2;
-	var mouse = {x:0,y:0}, player = {x:C.centerX,y:50,velx:0,vely:20,speed:3,rot:0}, move = {a:false,w:false,s:false,d:false};
-	var ctx = canvas.getContext("2d");
+	var mouse = {x:0,y:0}, player = {x:C.centerX,y:50,velx:0,vely:20,speed:3,rot:0,laser:false,fire:0,trigger:10,recoil:2,ammo:50,health:100}, move = {a:false,w:false,s:false,d:false};
+	var ctx = canvas.getContext("2d"); var mousepress = false; var HOpaque = 1.0; 
+	
+	var spr_player = new Image();
+	spr_player.src = "sprites/armguard1.png";
+	var spr_muzzle = new Image();
+	spr_muzzle.src = "sprites/flash.png";
+	var spr_flash = new Image();
+	spr_flash.src = "sprites/flashhalo.png";
+	
 	
 	setInterval(function() {
 		draw();
@@ -10,22 +18,30 @@
 		moving();
 		if(player.velx < 1 && player.velx > -1){ player.velx = 0 }
 		if(player.vely < 1 && player.vely > -1){ player.vely = 0 }
+		if(player.fire > 0){player.fire--}
 	},1000/C.fps)
 	
 	function draw(){
 		ctx.clearRect(0,0,C.width,C.height)
 		drawplayer(player.x,player.y,player.rot);
+		drawGUI()
 		
 		drawcursor(mouse.x,mouse.y);
 	}
 	
 	function drawcursor(x,y){
-		ctx.fillStyle = "rgba(255,255,255,0.8)"
+		ctx.fillStyle = "rgba(255,255,255,0.7)"
 		ctx.fillRect(x-1,y-1,2,2);
 		ctx.fillRect(x-10,y-1,6,2);
 		ctx.fillRect(x+10,y-1,-6,2);
 		ctx.fillRect(x-1,y-10,2,6);
 		ctx.fillRect(x-1,y+10,2,-6);
+	}
+	
+	function drawGUI() {
+		ctx.font = "bold 15px serif";
+		ctx.fillStyle = "rgba(255,255,255,0.7)"; if (player.ammo < 20){ ctx.fillStyle = "rgba(255,255,0,0.7)"; if (player.ammo < 10){ ctx.fillStyle = "rgba(255,50,0,0.8)"; } }
+		ctx.fillText(""+player.ammo,mouse.x+10,mouse.y-10)
 	}
 	
 	function drawplayer(x,y,r){
@@ -37,10 +53,39 @@
 		ctx.translate(x, y);
 		ctx.rotate(r);
 		ctx.fillStyle = "#34aaff"
-		ctx.fillRect(-10,-10,20,20);
-		ctx.fillRect(-10,-4,30,8);
-		ctx.fillStyle = laser
-		ctx.fillRect(10,-6,3000,2);
+		ctx.shadowColor = 'rgba(0,0,0,0.5)';
+		ctx.shadowBlur = 20;
+		ctx.shadowOffsetX = (player.x - C.centerX)/15;
+		ctx.shadowOffsetY = (player.y - C.centerY)/15;
+		ctx.drawImage(spr_player,-30,-20,60,40)
+		ctx.shadowColor = 'rgba(0,0,0,0)';
+		if(player.laser == true){
+			ctx.fillStyle = laser
+			ctx.fillRect(10,-6,3000,1);
+		}
+		if(player.fire == player.trigger){
+			ctx.fillStyle = "#fff8a6"
+			ctx.drawImage(spr_flash,-50,-100,200,200)
+			ctx.shadowColor = 'rgba(250,200,100,1)';
+			ctx.shadowBlur = 25;
+			ctx.shadowOffsetX = 0;
+			ctx.shadowOffsetY = 0;
+			ctx.fillRect(30,-1,3000,2);
+			ctx.drawImage(spr_muzzle,20,-20,60,40)
+			ctx.shadowColor = 'rgba(0,0,0,0)';
+		}
+		ctx.beginPath();
+		ctx.strokeStyle = "rgba(20,20,20,"+HOpaque+")";
+		ctx.arc(0, 0, 34, Math.PI/2, Math.PI*1+Math.PI/2, false);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.arc(0, 0, 34, Math.PI/2, Math.PI*(player.health/100)+Math.PI/2, false);
+		ctx.lineWidth = 2;
+		ctx.strokeStyle = "rgba(255,0,0,"+HOpaque+")";
+		ctx.stroke();
+		
+		if (HOpaque > 0.0) { HOpaque -= 0.005 }
+		
 		ctx.translate(0, 0);
 		ctx.restore();
 	}
@@ -58,6 +103,14 @@
 	canvas.onmousedown = function(){
 		mouseclick();
 	};
+	$('#mainc').on('mousedown mouseup', function mouseState(e) {
+		if (e.type == "mousedown") {
+			mousepress = true;console.log("hold")
+			mouseclick();
+		}else{
+			mousepress = false;console.log("unhold")
+		}
+	});
 	
 	window.onkeydown = function (e){
 		key = e.keyCode ? e.keyCode : e.which;
@@ -70,6 +123,8 @@
 				move.w = true; break;
 			case 83:
 				move.s = true; break;
+			case 82:
+				if(player.laser == false){player.laser = true}else{player.laser = false}
 		}
 	}
 	window.onkeyup = function (e){
@@ -119,4 +174,16 @@
 
 	function mouseclick(){
 		//When the mouse is clicked
+			if(player.fire < 1 && player.ammo > 0){
+				x = mouse.x-player.x; y = mouse.y-player.y;
+				player.fire = player.trigger
+				if (  x > y*2 && x < -y*2  )   {player.vely = player.recoil;}  //top triangle
+				else if (  x < y*2 && x > -y*2  )   {player.vely = -player.recoil;}  //bottom triangle
+				if (  -y > x*2 && -y < -x*2  )   {player.velx = player.recoil;}  //left triangle
+				else if(  -y < x*2 && -y > -x*2  )    {player.velx = -player.recoil;}  //right triangle
+				
+				var s_shot1 = new Audio("sounds/shot.wav");
+				s_shot1.play();
+				player.ammo--
+			}
 	};
